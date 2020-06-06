cd2ecd62846738d9e5a4454e3896ec9ca27660a9
Israel Rodrigues <israel.jesus@tqi.com.br>
2020-06-06 11:52:00 -0300

    initial commit

---
+++ src/main/java/br/com/paymybills/paymybills/PayMyBillsApplication.java
@@ -0,0 +1,13 @@
+package br.com.paymybills.paymybills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMyBillsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayMyBillsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/paymybills/paymybills/PayMyBillsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paymybills.paymybills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMyBillsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
