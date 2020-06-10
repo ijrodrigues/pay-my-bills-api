f49d6c751cef3ad6dfda718b186198c3f670d6a3
Israel Rodrigues <israel.jesus@tqi.com.br>
2020-06-10 19:23:07 -0300

    fix tests for roll back records to classes

--- src/test/java/br/com/paymybills/paymybills/domain/model/common/IdTest.java
+++ src/test/java/br/com/paymybills/paymybills/domain/model/common/IdTest.java
@@ -10,14 +10,14 @@ public class IdTest {
     @Test
     public void should_return_uuid_when_call_no_args_constructor() {
         var id = new Id();
-        assertNotNull(id.value());
-        assertEquals(36, id.value().length());
+        assertNotNull(id.getValue());
+        assertEquals(36, id.getValue().length());
     }
 
     @Test
     public void should_return_same_value_when_call_constructor_with_args() {
         var id = new Id("1234-1234");
-        assertNotNull(id.value());
-        assertEquals("1234-1234", id.value());
+        assertNotNull(id.getValue());
+        assertEquals("1234-1234", id.getValue());
     }
 }
